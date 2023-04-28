52ecf24ccb90155869a90a4bc7534b1b424a003d
chingistoktamyssov <125822105+chingistoktamyssov@users.noreply.github.com>
2023-04-28 13:33:20 -0400

    Add files via upload

---
+++ CCC '20 J4 - Cyclic Shifts.py
@@ -0,0 +1,15 @@
+t = input()
+s = input()
+cyclic = True
+
+for i in range(len(s)):
+  s += s[0]
+  s = s[1:]
+  shift = ''.join(str(feet) for feet in s)
+  if shift in t:
+    print("yes")
+    cyclic = False
+    break
+
+if cyclic:
+  print("no")
