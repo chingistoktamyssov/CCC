af834dabf17c5ce220a46ce973b95b8665388cdb
chingistoktamyssov <125822105+chingistoktamyssov@users.noreply.github.com>
2023-04-29 13:21:31 -0400

    Add files via upload

---
+++ '12/CCC '12 J3 - Icon Scaling.py
@@ -0,0 +1,10 @@
+k = int(input())
+
+for i in range(k):
+  print('*' * k + 'x' * k + '*' * k)
+
+for i in range(k):
+  print(' ' * k + 'x' * 2 * k)
+
+for i in range(k):
+  print('*' * k + ' ' * k + '*' * k)
