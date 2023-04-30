91c96367ddc191e7719826fd3c12abf7a6381204
chingistoktamyssov <125822105+chingistoktamyssov@users.noreply.github.com>
2023-04-30 13:26:27 -0400

    Delete CCC '23 J2 - Chili Peppers.py

--- CCC '23 J2 - Chili Peppers.py
+++
@@ -1,7 +0,0 @@
-peppers = {'Poblano': 1500, 'Mirasol': 6000, 'Serrano': 15500, 'Cayenne': 40000, 'Thai': 75000, 'Habanero': 125000}
-
-heatlevel = 0
-for i in range(int(input())):
-  heatlevel = heatlevel + peppers[input()]
-  
-print(heatlevel)
