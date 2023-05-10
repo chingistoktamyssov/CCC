f1de04a1b6f2cb1bfd186877be3cb5f65280160f
chingistoktamyssov <125822105+chingistoktamyssov@users.noreply.github.com>
2023-05-10 16:08:45 -0400

    Add files via upload

---
+++ '06/CCC '06 J5 - CCC Othello.py
@@ -0,0 +1,487 @@
+a = {
+  (1, 1): 'E',
+  (1, 2): 'E',
+  (1, 3): 'E',
+  (1, 4): 'E',
+  (1, 5): 'E',
+  (1, 6): 'E',
+  (1, 7): 'E',
+  (1, 8): 'E',
+  (2, 1): 'E',
+  (2, 2): 'E',
+  (2, 3): 'E',
+  (2, 4): 'E',
+  (2, 5): 'E',
+  (2, 6): 'E',
+  (2, 7): 'E',
+  (2, 8): 'E',
+  (3, 1): 'E',
+  (3, 2): 'E',
+  (3, 3): 'E',
+  (3, 4): 'E',
+  (3, 5): 'E',
+  (3, 6): 'E',
+  (3, 7): 'E',
+  (3, 8): 'E',
+  (4, 1): 'E',
+  (4, 2): 'E',
+  (4, 3): 'E',
+  (4, 4): 'W',
+  (4, 5): 'B',
+  (4, 6): 'E',
+  (4, 7): 'E',
+  (4, 8): 'E',
+  (5, 1): 'E',
+  (5, 2): 'E',
+  (5, 3): 'E',
+  (5, 4): 'B',
+  (5, 5): 'W',
+  (5, 6): 'E',
+  (5, 7): 'E',
+  (5, 8): 'E',
+  (6, 1): 'E',
+  (6, 2): 'E',
+  (6, 3): 'E',
+  (6, 4): 'E',
+  (6, 5): 'E',
+  (6, 6): 'E',
+  (6, 7): 'E',
+  (6, 8): 'E',
+  (7, 1): 'E',
+  (7, 2): 'E',
+  (7, 3): 'E',
+  (7, 4): 'E',
+  (7, 5): 'E',
+  (7, 6): 'E',
+  (7, 7): 'E',
+  (7, 8): 'E',
+  (8, 1): 'E',
+  (8, 2): 'E',
+  (8, 3): 'E',
+  (8, 4): 'E',
+  (8, 5): 'E',
+  (8, 6): 'E',
+  (8, 7): 'E',
+  (8, 8): 'E'
+}
+b = {
+  (1, 1): 'B',
+  (1, 2): 'E',
+  (1, 3): 'E',
+  (1, 4): 'E',
+  (1, 5): 'E',
+  (1, 6): 'E',
+  (1, 7): 'E',
+  (1, 8): 'W', 
+  (2, 1): 'E',
+  (2, 2): 'B',
+  (2, 3): 'E',
+  (2, 4): 'E',
+  (2, 5): 'E',
+  (2, 6): 'E',
+  (2, 7): 'W',
+  (2, 8): 'E',
+  (3, 1): 'E',
+  (3, 2): 'E',
+  (3, 3): 'B',
+  (3, 4): 'E',
+  (3, 5): 'E',
+  (3, 6): 'W',
+  (3, 7): 'E',
+  (3, 8): 'E',
+  (4, 1): 'E',
+  (4, 2): 'E',
+  (4, 3): 'E',
+  (4, 4): 'B',
+  (4, 5): 'W',
+  (4, 6): 'E',
+  (4, 7): 'E',
+  (4, 8): 'E',
+  (5, 1): 'E',
+  (5, 2): 'E',
+  (5, 3): 'E',
+  (5, 4): 'W',
+  (5, 5): 'B',
+  (5, 6): 'E',
+  (5, 7): 'E',
+  (5, 8): 'E',
+  (6, 1): 'E',
+  (6, 2): 'E',
+  (6, 3): 'W',
+  (6, 4): 'E',
+  (6, 5): 'E',
+  (6, 6): 'B',
+  (6, 7): 'E',
+  (6, 8): 'E',
+  (7, 1): 'E',
+  (7, 2): 'W',
+  (7, 3): 'E',
+  (7, 4): 'E',
+  (7, 5): 'E',
+  (7, 6): 'E',
+  (7, 7): 'B',
+  (7, 8): 'E',
+  (8, 1): 'W', 
+  (8, 2): 'E',
+  (8, 3): 'E',
+  (8, 4): 'E',
+  (8, 5): 'E',
+  (8, 6): 'E',
+  (8, 7): 'E',
+  (8, 8): 'B'
+}
+c = {
+  (1, 1): 'E',
+  (1, 2): 'E',
+  (1, 3): 'B',
+  (1, 4): 'B',
+  (1, 5): 'W',
+  (1, 6): 'W',
+  (1, 7): 'E',
+  (1, 8): 'E',
+  (2, 1): 'E',
+  (2, 2): 'E',
+  (2, 3): 'B',
+  (2, 4): 'B',
+  (2, 5): 'W',
+  (2, 6): 'W',
+  (2, 7): 'E',
+  (2, 8): 'E',
+  (3, 1): 'E',
+  (3, 2): 'E',
+  (3, 3): 'B',
+  (3, 4): 'B',
+  (3, 5): 'W',
+  (3, 6): 'W',
+  (3, 7): 'E',
+  (3, 8): 'E',
+  (4, 1): 'E',
+  (4, 2): 'E',
+  (4, 3): 'B',
+  (4, 4): 'B',
+  (4, 5): 'W',
+  (4, 6): 'W',
+  (4, 7): 'E',
+  (4, 8): 'E',
+  (5, 1): 'E',
+  (5, 2): 'E',
+  (5, 3): 'B',
+  (5, 4): 'B',
+  (5, 5): 'W',
+  (5, 6): 'W',
+  (5, 7): 'E',
+  (5, 8): 'E',
+  (6, 1): 'E',
+  (6, 2): 'E',
+  (6, 3): 'B',
+  (6, 4): 'B',
+  (6, 5): 'W',
+  (6, 6): 'W',
+  (6, 7): 'E',
+  (6, 8): 'E',
+  (7, 1): 'E',
+  (7, 2): 'E',
+  (7, 3): 'B',
+  (7, 4): 'B',
+  (7, 5): 'W',
+  (7, 6): 'W',
+  (7, 7): 'E',
+  (7, 8): 'E',
+  (8, 1): 'E',
+  (8, 2): 'E',
+  (8, 3): 'B',
+  (8, 4): 'B',
+  (8, 5): 'W',
+  (8, 6): 'W',
+  (8, 7): 'E',
+  (8, 8): 'E'
+}
+
+def blackturn(board, x):
+
+  board[tuple(x)] = 'B'
+
+  # up
+  diff = 1
+  tochange = []
+  while x[0] - diff >= 1:
+    if board[(x[0] - diff, x[1])] == 'B':
+      for i in tochange:
+        board[tuple(i)] = 'B'
+      break
+    elif board[(x[0] - diff, x[1])] == 'W':
+      tochange.append([x[0] - diff, x[1]])
+      diff += 1
+    else:
+      diff += 1
+
+  # up right  
+  diff = 1
+  tochange = []
+  while x[0] - diff >= 1 and x[1] + diff <= 8:
+    if board[(x[0] - diff, x[1] + diff)] == 'B':
+      for i in tochange:
+        board[tuple(i)] = 'B'
+      break
+    elif board[(x[0] - diff, x[1] + diff)] == 'W':
+      tochange.append([x[0] - diff, x[1] + diff])
+      diff += 1
+    else:
+      diff += 1
+
+  # right
+  diff = 1
+  tochange = []
+  while x[1] + diff <= 8:
+    if board[(x[0], x[1] + diff)] == 'B':
+      for i in tochange:
+        board[tuple(i)] = 'B'
+      break
+    elif board[(x[0], x[1] + diff)] == 'W':
+      tochange.append([x[0], x[1] + diff])
+      diff += 1
+    else:
+      diff += 1
+
+  # down right
+  diff = 1
+  tochange = []
+  while x[0] + diff <= 8 and x[1] + diff <= 8:
+    if board[(x[0] + diff, x[1] + diff)] == 'B':
+      for i in tochange:
+        board[tuple(i)] = 'B'
+      break
+    elif board[(x[0] + diff, x[1] + diff)] == 'W':
+      tochange.append([x[0] + diff, x[1] + diff])
+      diff += 1
+    else:
+      diff += 1
+
+  # down
+  diff = 1
+  tochange = []
+  while x[0] + diff <= 8:
+    if board[(x[0] + diff, x[1])] == 'B':
+      for i in tochange:
+        board[tuple(i)] = 'B'
+      break
+    elif board[(x[0] + diff, x[1])] == 'W':
+      tochange.append([x[0] + diff, x[1]])
+      diff += 1
+    else:
+      diff += 1
+
+  # down left
+  diff = 1
+  tochange = []
+  while x[0] + diff <= 8 and x[1] - diff >= 1:
+    if board[(x[0] + diff, x[1] - diff)] == 'B':
+      for i in tochange:
+        board[tuple(i)] = 'B'
+      break
+    elif board[(x[0] + diff, x[1] - diff)] == 'W':
+      tochange.append([x[0] + diff, x[1] - diff])
+      diff += 1
+    else:
+      diff += 1
+
+  # left
+  diff = 1
+  tochange = []
+  while x[1] - diff >= 1:
+    if board[(x[0], x[1] - diff)] == 'B':
+      for i in tochange:
+        board[tuple(i)] = 'B'
+      break
+    elif board[(x[0], x[1] - diff)] == 'W':
+      tochange.append([x[0], x[1] - diff])
+      diff += 1
+    else:
+      diff += 1
+
+  # up left
+  diff = 1
+  tochange = []
+  while x[0] - diff >= 1 and x[1] - diff >= 1:
+    if board[(x[0] - diff, x[1] - diff)] == 'B':
+      for i in tochange:
+        board[tuple(i)] = 'B'
+      break
+    elif board[(x[0] - diff, x[1] - diff)] == 'W':
+      tochange.append([x[0] - diff, x[1] - diff])
+      diff += 1
+    else:
+      diff += 1
+      
+  return board
+
+def whiteturn(board, x):
+
+  board[tuple(x)] = 'W'
+
+  # up
+  diff = 1
+  tochange = []
+  while x[0] - diff >= 1:
+    if board[(x[0] - diff, x[1])] == 'W':
+      for i in tochange:
+        board[tuple(i)] = 'W'
+      break
+    elif board[(x[0] - diff, x[1])] == 'B':
+      tochange.append([x[0] - diff, x[1]])
+      diff += 1
+    else:
+      diff += 1
+
+  # up right  
+  diff = 1
+  tochange = []
+  while x[0] - diff >= 1 and x[1] + diff <= 8:
+    if board[(x[0] - diff, x[1] + diff)] == 'W':
+      for i in tochange:
+        board[tuple(i)] = 'W'
+      break
+    elif board[(x[0] - diff, x[1] + diff)] == 'B':
+      tochange.append([x[0] - diff, x[1] + diff])
+      diff += 1
+    else:
+      diff += 1
+
+  # right
+  diff = 1
+  tochange = []
+  while x[1] + diff <= 8:
+    if board[(x[0], x[1] + diff)] == 'W':
+      for i in tochange:
+        board[tuple(i)] = 'W'
+      break
+    elif board[(x[0], x[1] + diff)] == 'B':
+      tochange.append([x[0], x[1] + diff])
+      diff += 1
+    else:
+      diff += 1
+
+  # down right
+  diff = 1
+  tochange = []
+  while x[0] + diff <= 8 and x[1] + diff <= 8:
+    if board[(x[0] + diff, x[1] + diff)] == 'W':
+      for i in tochange:
+        board[tuple(i)] = 'W'
+      break
+    elif board[(x[0] + diff, x[1] + diff)] == 'B':
+      tochange.append([x[0] + diff, x[1] + diff])
+      diff += 1
+    else:
+      diff += 1
+
+  # down
+  diff = 1
+  tochange = []
+  while x[0] + diff <= 8:
+    if board[(x[0] + diff, x[1])] == 'W':
+      for i in tochange:
+        board[tuple(i)] = 'W'
+      break
+    elif board[(x[0] + diff, x[1])] == 'B':
+      tochange.append([x[0] + diff, x[1]])
+      diff += 1
+    else:
+      diff += 1
+
+  # down left
+  diff = 1
+  tochange = []
+  while x[0] + diff <= 8 and x[1] - diff >= 1:
+    if board[(x[0] + diff, x[1] - diff)] == 'W':
+      for i in tochange:
+        board[tuple(i)] = 'W'
+      break
+    elif board[(x[0] + diff, x[1] - diff)] == 'B':
+      tochange.append([x[0] + diff, x[1] - diff])
+      diff += 1
+    else:
+      diff += 1
+
+  # left
+  diff = 1
+  tochange = []
+  while x[1] - diff >= 1:
+    if board[(x[0], x[1] - diff)] == 'W':
+      for i in tochange:
+        board[tuple(i)] = 'W'
+      break
+    elif board[(x[0], x[1] - diff)] == 'B':
+      tochange.append([x[0], x[1] - diff])
+      diff += 1
+    else:
+      diff += 1
+
+  # up left
+  diff = 1
+  tochange = []
+  while x[0] - diff >= 1 and x[1] - diff >= 1:
+    if board[(x[0] - diff, x[1] - diff)] == 'W':
+      for i in tochange:
+        board[tuple(i)] = 'W'
+      break
+    elif board[(x[0] - diff, x[1] - diff)] == 'B':
+      tochange.append([x[0] - diff, x[1] - diff])
+      diff += 1
+    else:
+      diff += 1
+      
+  return board
+
+line = input().split()
+turn = 'B'
+moves = []
+for i in range(1, int(line[1]) + 1):
+  moves.append([int(line[2 * i]), int(line[2 * i + 1])])
+
+bp = 0
+wp = 0
+
+if line[0] == 'a':
+  for i in moves:
+    if turn == 'B':
+      a = blackturn(a, i)
+      turn = 'W'
+    else:
+      a = whiteturn(a, i)
+      turn = 'B'
+  for i in a:
+    if a[i] == 'B':
+      bp += 1
+    elif a[i] == 'W':
+      wp += 1
+      
+elif line[0] == 'b':
+  for i in moves:
+    if turn == 'B':
+      b = blackturn(b, i)
+      turn = 'W'
+    else:
+      b = whiteturn(b, i)
+      turn = 'B'
+  for i in b:
+    if b[i] == 'B':
+      bp += 1
+    elif b[i] == 'W':
+      wp += 1
+      
+else:
+  for i in moves:
+    if turn == 'B':
+      c = blackturn(c, i)
+      turn = 'W'
+    else:
+      c = whiteturn(c, i)
+      turn = 'B'
+  for i in c:
+    if c[i] == 'B':
+      bp += 1
+    elif c[i] == 'W':
+      wp += 1
+
+print(bp, wp)
