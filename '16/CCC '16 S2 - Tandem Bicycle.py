3cb144e1f6ed79625961df93baefcd61bfb2b06c
chingistoktamyssov <125822105+chingistoktamyssov@users.noreply.github.com>
2023-06-21 10:54:18 -0400

    Add files via upload

---
+++ '16/CCC '16 S2 - Tandem Bicycle.py
@@ -0,0 +1,18 @@
+question = int(input())
+n = int(input())
+c1 = sorted([int(x) for x in input().split()])
+c2 = sorted([int(x) for x in input().split()])
+
+total = 0
+
+if question == 1:
+  for i in range(n):
+    speed = max(c1[i], c2[i])
+    total += speed
+
+else:
+  for i in range(n):
+    speed = max(c1[i], c2[-(i+1)])
+    total += speed
+
+print(total)
